3cc10ad1e6552840bd04265ee45ce5a1ab91fabe
saddler76 <44097319+saddler76@users.noreply.github.com>
2019-06-06 17:46:56 +0200

    Update main5.js

--- scripts/main5.js
+++ scripts/main5.js
@@ -1,6 +1,16 @@
-function validateForm() {
+function validateForm()  {
+ 
   var x = document.forms["formA"]["firstname"].value;
-  if (x == "") {
+  if (x==null || x=="")
+     {
+    alert("Popunite polje");
+    return false;
+  }
+}
+
+  var y = document.forms["formA"]["location"].value;
+  if (y==null || y=="")
+     {
     alert("Popunite polje");
     return false;
   }
