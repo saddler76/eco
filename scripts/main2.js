76bace08ca709980e93004dc7daffb4ae39573d4
saddler76 <44097319+saddler76@users.noreply.github.com>
2019-06-06 17:28:13 +0200

    Update main2.js

--- scripts/main2.js
+++ scripts/main2.js
@@ -1,5 +1,5 @@
 function validateForm() {
-  var x = document.forms["formA"]["firstname"]["location"].value;
+  var x = document.forms["formA"]["firstname"].value;
   if (x == "") {
     alert("Popunite polje");
     return false;
